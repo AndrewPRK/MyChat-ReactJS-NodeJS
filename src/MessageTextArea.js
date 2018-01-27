14f10cbe2a617bcf4b656f739398ef561e144447
Andrew <Andyhaprk@gmail.com>
2018-01-27 20:18:25 +0200

    Add onEnterPressEvent

--- src/MessageTextArea.js
+++ src/MessageTextArea.js
@@ -11,6 +11,7 @@ import {sendMessage} from "./AC"
         }
         this.handleChange=this.handleChange.bind(this);
         this.handleClick=this.handleClick.bind(this);
+        this.onEnterPress=this.onEnterPress.bind(this);
     }
     handleChange(event)
     {
@@ -27,6 +28,16 @@ import {sendMessage} from "./AC"
         });
         event.preventDefault();
     }
+    onEnterPress(event)
+    {
+        if(event.keyCode == 13 && event.shiftKey == false) {
+            this.props.sendMessage(this.state.message);
+            this.setState({
+                message:""
+            });
+            event.preventDefault();
+        }
+    }
     render() {
         return (
             <div className="chat-message clearfix">
@@ -34,7 +45,8 @@ import {sendMessage} from "./AC"
                 name="message-to-send" id="message-to-send" 
                 placeholder="Type your message" rows="3"
                 value={this.state.message}
-                onChange={this.handleChange}>
+                onChange={this.handleChange}
+                onKeyDown={this.onEnterPress}>
                 </textarea>
                 <i className="fa fa-file-o"></i> &nbsp;&nbsp;&nbsp;
             <i className="fa fa-file-image-o"></i>
